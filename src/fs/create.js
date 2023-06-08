65a1de22ee1bab9523771e1db4ac90d838244e4b
Yuri Skripal <yjura@protonmail.com>
2023-06-08 20:16:40 +0300

    feat: create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { writeFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, '/files', 'fresh.txt');
+const content = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await writeFile(filePath, content, { flag: 'wx' });
+  } catch (err) {
+    if (err.code === 'EEXIST') {
+      throw new Error('FS operation failed');
+    }
+    console.log(err)
+  }
 };
 
 await create();
