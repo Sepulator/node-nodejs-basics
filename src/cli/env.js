a300e1df8485bf1ad31840675300aabf7972c1af
Yuri Skripal <yjura@protonmail.com>
2023-06-09 16:33:22 +0300

    feat: display env RSS

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  const keys = Object.keys(process.env).filter((key) => key.includes('RSS'));
+
+  console.table(keys.map((key)=> {
+    return {
+      key: key,
+      value: process.env[key]
+    }
+  }))
 };
 
 parseEnv();
