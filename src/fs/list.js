947b99d93a1c26376616c26c2e6a09648a0678a9
Yuri Skripal <yjura@protonmail.com>
2023-06-09 14:52:48 +0300

    feat: add list files

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,26 @@
+import { promises as fs } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dirPath = path.join(__dirname, '/files');
+
+const fileExists = async (path) => !!(await fs.stat(path).catch((e) => false));
+
 const list = async () => {
-    // Write your code here 
+  const isDirExist = await fileExists(dirPath);
+  if (!isDirExist) throw new Error('FS operation failed');
+  const files = await fs.readdir(dirPath);
+  console.table(
+    files.map((file) => {
+      const ext = path.extname(file);
+      return {
+        'File Name': path.basename(file, ext),
+        'Extension': ext,
+      };
+    })
+  );
 };
 
 await list();
