0770b5344ec14370696d13edf39978485227bcb7
Yuri Skripal <yjura@protonmail.com>
2023-06-09 11:58:17 +0300

    feat: add copy files

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,35 @@
+import { cp, access, constants, stat } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const sourcePath = path.join(__dirname, '/files');
+const destPath = path.join(__dirname, '/files_copy');
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await access(sourcePath, constants.R_OK | constants.W_OK);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+    console.log(err);
+  }
+
+  try {
+    const dir = await stat(destPath);
+    if (dir.isDirectory()) {
+      throw new Error('FS operation failed');
+    }
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      cp(sourcePath, destPath, { recursive: true });
+      console.log('Files successfully copied');
+    } else {
+      console.log(err);
+    }
+  }
 };
 
 await copy();
