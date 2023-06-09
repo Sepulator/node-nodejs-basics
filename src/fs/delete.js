8677036841a9b9eeb024057b977397480c5a5451
Yuri Skripal <yjura@protonmail.com>
2023-06-09 14:23:44 +0300

    feat: add delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import { promises as fs } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, '/files', 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+  await fs
+    .unlink(filePath)
+    .then(() => console.log('File deleted successfully.'))
+    .catch((e) => {
+      throw new Error('FS operation failed');
+    });
 };
 
 await remove();
