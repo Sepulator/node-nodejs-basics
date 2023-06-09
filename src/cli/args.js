bb6ca7741d446dbbbfd1cd0d0f3360e45a12eb2a
Yuri Skripal <yjura@protonmail.com>
2023-06-09 17:44:26 +0300

    feat: display cli args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+
+  if (process.argv.length === 2) {
+    console.error('Expected at least one argument!');
+    process.exit(1);
+  }
+
+  const pairs = process.argv.slice(2);
+
+  for (let i = 0; i < pairs.length; i = i + 2) {
+    const propName = pairs[i].slice(2);
+    const value = pairs[i + 1];
+    console.log(`${propName} is ${value}`);
+  }
 };
 
 parseArgs();
