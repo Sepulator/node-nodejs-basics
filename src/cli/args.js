2f99c5799b00d999a065fb7923e2189acce9e7a6
Yuri Skripal <yjura@protonmail.com>
2023-06-10 11:21:08 +0300

    feat: add modules & env

--- src/cli/args.js
+++ src/cli/args.js
@@ -8,9 +8,9 @@ const parseArgs = () => {
   const pairs = process.argv.slice(2);
 
   for (let i = 0; i < pairs.length; i = i + 2) {
-    const propName = pairs[i].slice(2);
+    const prop = pairs[i].slice(2);
     const value = pairs[i + 1];
-    console.log(`${propName} is ${value}`);
+    console.log(`${prop} is ${value}`);
   }
 };
 
