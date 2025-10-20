79d8b8713f106c1fc48a4d6b71cb5dd241ea2e80
Yuri Skripal <yjura@protonmail.com>
2025-10-20 18:41:17 +0300

    feat: add create file function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
-const create = async () => {
-  // Write your code here
+import { writeFile } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const fileName = 'fresh.txt';
+const text = 'I am fresh and young';
+const folder = 'files';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const create = async (fileName, folderName, text) => {
+  try {
+    const folderPath = join(__dirname, folderName);
+    const filePath = join(folderPath, fileName);
+
+    await writeFile(filePath, text, 'utf-8');
+
+    console.log(`✅ Successfully wrote to ${filePath}`);
+  } catch (error) {
+    console.error(`FS operation failed`);
+  }
 };
 
-await create();
+await create(fileName, folder, text);
