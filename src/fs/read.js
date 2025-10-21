2f2438f396a89fd6bde6d0d1ead52261cba84690
Yuri Skripal <yjura@protonmail.com>
2025-10-21 13:38:49 +0300

    feat: add read file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
-const read = async () => {
-  // Write your code here
+import { readFile } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
+const read = async (filePath) => {
+  try {
+    const content = await readFile(filePath, { encoding: 'utf-8', flag: 'r' });
+    console.log(content);
+  } catch (error) {
+    console.error(`⚠️ FS operation failed: ${error.message}`);
+  }
 };
 
-await read();
+await read(filePath);
