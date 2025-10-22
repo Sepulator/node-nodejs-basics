fd078e329c90a81e846c45d5469d337b965d2877
Yuri Skripal <yjura@protonmail.com>
2025-10-22 12:29:52 +0300

    feat: add parse environment variables function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
 const parseEnv = () => {
-  // Write your code here
+  const vars = [];
+
+  for (const key in process.env) {
+    if (key.startsWith('RSS_')) {
+      vars.push(`${key}=${process.env[key]}`);
+    }
+  }
+
+  if (vars.length > 0) {
+    const output = vars.join('; ');
+    console.log(`✅ ${output}`);
+  } else {
+    console.log('⚠️ No environment variables with prefix RSS_.');
+  }
 };
 
 parseEnv();
