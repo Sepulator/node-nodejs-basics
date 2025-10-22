162eb3f4f2e0a7c9b1ff4dfa30ed21b3e1bb33ef
Yuri Skripal <yjura@protonmail.com>
2025-10-22 19:15:04 +0300

    feat: add transorm function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+import { stdin, stdout } from 'process';
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
 const transform = async () => {
-  // Write your code here
+  const reverse = new Transform({
+    transform(data, enc, cb) {
+      const reversedData =
+        data.toString().split('').reverse('').join('') + '\n';
+      cb(null, reversedData);
+    },
+  });
+
+  try {
+    pipeline(stdin, reverse, stdout);
+  } catch (error) {
+    console.error(`⚠️Error transforming stream: ${error.message}`);
+  }
 };
 
 await transform();
