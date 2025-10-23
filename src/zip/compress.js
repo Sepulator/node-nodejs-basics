7c7d782793734c6d86b7dd5feef0dc066c34f2bc
Yuri Skripal <yjura@protonmail.com>
2025-10-23 12:57:33 +0300

    feat: add compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
-const compress = async () => {
-  // Write your code here
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePathSource = join(__dirname, 'files', 'fileToCompress.txt');
+const filePathDestination = join(__dirname, 'files', 'archive.gz');
+
+const compress = async (filePathSource, filePathDestination) => {
+  try {
+    const gzip = createGzip();
+    const source = createReadStream(filePathSource);
+    const destination = createWriteStream(filePathDestination, { flags: 'w' });
+    await pipeline(source, gzip, destination);
+    console.log('✅ File successfully compressed');
+  } catch (error) {
+    console.error(`⚠️Error transforming stream: ${error.message}`);
+  }
 };
 
-await compress();
+await compress(filePathSource, filePathDestination);
