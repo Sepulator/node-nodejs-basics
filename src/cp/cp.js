722796b4f9c9885d6fb9c6ea06bdd600c5626f30
Yuri Skripal <yjura@protonmail.com>
2025-10-23 14:30:03 +0300

    feat: add worker threads

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+import { Worker } from 'worker_threads';
+import { availableParallelism } from 'os';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePathWorker = join(__dirname, 'worker.js');
+
 const spawnChildProcess = async (args) => {
   // Write your code here
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(/* [someArgument1, someArgument2, ...] */);
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,50 @@
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+import { Worker } from 'worker_threads';
+import { availableParallelism } from 'os';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePathWorker = join(__dirname, 'worker.js');
+
+const START_NUMBER = 10;
+const THREADS_NUMBER = availableParallelism();
+
+const startWorker = async (n) => {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(filePathWorker, {
+      workerData: { n },
+    });
+
+    worker.on('message', (message) => {
+      resolve({ status: 'resolved', data: message.result });
+    });
+
+    worker.on('error', () => {
+      reject({ status: 'error', data: null });
+    });
+  });
+};
+
 const performCalculations = async () => {
-  // Write your code here
+  const workerPromises = [];
+
+  for (let i = 0; i < THREADS_NUMBER; i++) {
+    workerPromises.push(startWorker(START_NUMBER + i));
+  }
+
+  try {
+    const results = await Promise.allSettled(workerPromises);
+
+    console.table(
+      results.map((result) => ({
+        status: result.value.status,
+        data: result.value.data,
+      }))
+    );
+  } catch (error) {
+    console.error(`Error performing calculations: ${error.message}`);
+  }
 };
 
 await performCalculations();
